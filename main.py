c40b748957606c011898d920ea61c90cf6804d5d
Rudaeva V. <vrudaeva1@gmail.com>
2025-02-18 20:34:08 +0300

    test commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import logging, asyncio
+import logging
+import asyncio
 from aiogram import Dispatcher, Bot
 from config_data.config import load_config
 from handlers.user import user_router
@@ -6,6 +7,9 @@ from handlers.admin import admin_router
 from middleware.outer import AdminOuterMiddleware, DataBaseSession
 from FSM.fsm import storage
 from database.engine_db import session_maker, create_db, drop_db
+
+
+# Что-то новое
 
 
 logger = logging.getLogger(__name__)
@@ -20,14 +24,14 @@ async def main():
     logging.info('Starting bot')
 
     config = load_config()
-    
+
     bot = Bot(token=config.tg_bot.token)
     admin_ids = list(config.tg_bot.admin_ids)
     bot.admin_ids = admin_ids
     dp = Dispatcher(storage=storage)
 
     async def startup(bot):
-        #await drop_db()
+        # await drop_db()
         await create_db()
 
     dp.startup.register(startup)
@@ -35,7 +39,8 @@ async def main():
     dp.include_router(user_router)
     dp.include_router(admin_router)
 
-    admin_router.message.outer_middleware(AdminOuterMiddleware(admin_ids=admin_ids))
+    admin_router.message.outer_middleware(
+        AdminOuterMiddleware(admin_ids=admin_ids))
     dp.update.middleware(DataBaseSession(session_maker))
 
     await bot.delete_webhook(drop_pending_updates=True)
